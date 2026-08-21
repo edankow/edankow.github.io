acf31d5581f8f8fd702f8d568babcb0f76502278
edankow <256715608+edankow@users.noreply.github.com>
2026-08-21 16:35:20 -0400

    Update outgroup_taxa.js

--- outgroup_taxa.js
+++ outgroup_taxa.js
@@ -1,5 +1,15 @@
 const outgroupTaxa = Object.freeze([
   {
+    "taxonId": 47822,
+    "name": "Diptera",
+    "rank": "order"
+  },
+  {
+    "taxonId": 342767,
+    "name": "Calyptratae",
+    "rank": "zoosubsection"
+  },
+  {
     "taxonId": 356157,
     "name": "Oestroidea",
     "rank": "superfamily"
