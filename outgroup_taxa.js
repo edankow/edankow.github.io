137cc376fd678f9fb3762b24ec8455f4f7c9a027
edankow <256715608+edankow@users.noreply.github.com>
2026-08-21 16:17:19 -0400

    added outgroup shortcuts for other IDs

---
+++ outgroup_taxa.js
@@ -0,0 +1,17 @@
+const outgroupTaxa = Object.freeze([
+  {
+    "taxonId": 356157,
+    "name": "Oestroidea",
+    "rank": "superfamily"
+  },
+  {
+    "taxonId": 70144,
+    "name": "Muscidae",
+    "rank": "family"
+  },
+  {
+    "taxonId": 129218,
+    "name": "Neomyia cornicina",
+    "rank": "species"
+  }
+]);
